ec860194f69e1fd9a54ea49e873c783cb6a7ed52
Emrah3865 <81292068+Emrah3865@users.noreply.github.com>
2024-11-18 10:08:19 +0100

    header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'about', component: AboutMeComponent },
+  { path: 'skills', component: MySkillsComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: '', redirectTo: '/about', pathMatch: 'full' },
+];
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+  imports: [CommonModule],
+})
+export class HeaderComponent {
+  activeSection: string = 'about';
+
+  setActive(section: string) {
+    this.activeSection = section;
+  }
+}
