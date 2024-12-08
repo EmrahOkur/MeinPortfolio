3400562bcf7e91f523a64431af0ac24309689713
Emrah3865 <81292068+Emrah3865@users.noreply.github.com>
2024-12-08 15:23:02 +0100

    Implement responsive burger menu and animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-
+import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ReferencesComponent } from './references/references.component';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -12,7 +17,13 @@ import { FooterComponent } from './footer/footer.component';
     RouterOutlet,
     RouterModule,
     HeaderComponent,
+    AboveTheFoldComponent,
+    AboutMeComponent,
     FooterComponent,
+    ContactMeComponent,
+    MySkillsComponent,
+    PortfolioComponent,
+    ReferencesComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
--- src/app/contact-me/contact-me.component.ts
+++ src/app/contact-me/contact-me.component.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import {
+  ReactiveFormsModule,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 @Component({
   selector: 'app-contact-me',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './contact-me.component.html',
-  styleUrl: './contact-me.component.scss'
+  styleUrl: './contact-me.component.scss',
 })
 export class ContactMeComponent {
+  contactForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', [Validators.required, Validators.minLength(10)]],
+      consent: [false, Validators.requiredTrue],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.contactForm.valid) {
+      alert('Form submitted successfully!');
+    } else {
+      alert('Please fill out all fields correctly.');
+    }
+  }
+
+  get name() {
+    return this.contactForm.get('name');
+  }
+  get email() {
+    return this.contactForm.get('email');
+  }
+  get message() {
+    return this.contactForm.get('message');
+  }
+  get consent() {
+    return this.contactForm.get('consent');
+  }
 }
